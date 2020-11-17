448875cc94268a8d12f9f9ec588d257507cd20a9
Ahmed 2 <Ahmed 2@DESKTOP-OAOPJ5C.home>
2020-11-17 12:50:00 -0800

    First Commit

---
+++ Dar Banking/src/DarBanking.java
@@ -0,0 +1,13 @@
+
+public class DarBanking {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	Account ahmed = new Account("Ahmed Dar", "A001");
+	ahmed.showMenu();
+	
+	}
+}
+
+	
